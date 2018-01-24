93169168aaf33eb258c8c7262666c258a29abcd5
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-01-24 23:40:50 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Index</title>
+</head>
+
+<body>
+	 <?php echo '<p>Hello my lovely world this is my first git project.</p>'; ?>
+</body>
+</html>
