6ed122cb1c4314e2a4f658b1c61c505a38e84ebb
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-02-11 17:32:10 +0000

    Added login form

--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="robots" contents="follow">
-    
 <title>Index</title>
-<<<<<<< HEAD
-    
-    <base href="/" />
-    <!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />    
-=======
     <base href="/" />
     <!-- UIkit CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />
@@ -20,43 +10,9 @@
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
->>>>>>> 393cd35910814ad36f57d5bcc7a6949004627861
 </head>
 
 <body>
-       
-	 <div class="uk-section uk-container">
-    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
-                <form class="uk-form-stacked js-login">
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text">Email</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text-1">Password</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text-1" type="password" required="required" placeholder="Your Password">
-        </div>
-    </div>
-                    
-    <div class="uk-margin">
-        <button class="uk-button uk-button-default" type="submit">Login</button>
-    </div>                
-
-</form>
-        </div>
-   </div>                                                        
-
-    <!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />
-
-<!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
-<script src="http://code.jquery.com/jquery-3.3.1.js"></script>                                                                                              
+	 <?php echo '<p>Well it is all now finally working. Let see it if goes to git repository</p>'; ?>
 </body>
 </html>
