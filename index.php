3b0c54b3a6b297964fa4c97fcd749dd590437e8b
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-02-11 14:43:26 +0000

    After adding jquery

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 <head>
 <meta charset="utf-8">
 <title>Index</title>
+    <base href="/" />
+    <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />    
 </head>
 
 <body>
-	 <?php echo '<p>Well it is all now finally working. Let see it if goes to git repository</p>'; ?>
+    <!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
+<script
+			  src="http://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+    
+	 <?php echo '<p>Maybe Well it is all now finally working. Let see it if goes to git repository</p>'; ?>
 </body>
 </html>
