54a719633645de12559c911d362db1a6aa1ddfb6
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-02-11 14:07:57 +0000

    After adding uikit.css

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <head>
 <meta charset="utf-8">
 <title>Index</title>
+    <base href="/" />
+    <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
 </head>
 
 <body>
