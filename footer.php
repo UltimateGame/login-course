0e665ea6a761e43368d0f21517bcb8265bb441d2
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-02-14 14:06:03 +0000

    created config and footer pages

---
+++ footer.php
@@ -0,0 +1,7 @@
+ <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.js"></script>                                                                
