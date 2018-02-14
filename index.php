043be6a5fb7fe479f14de89bcd9408575e2c9354
Neil Hooper <34611300+UltimateGame@users.noreply.github.com>
2018-02-14 14:02:36 +0000

    Removed login form and added register.php form

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+
+// Allow the config
+	define('_CONFIG_', true);
+// Require the config
+	require_once "inc/config.php";
+ ?>
+
 <!doctype html>
 <html>
 <head>
@@ -18,37 +26,19 @@
 <body>
        
 	 <div class="uk-section uk-container">
-    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
-                <form class="uk-form-stacked js-login">
+    
+    <?php
+    echo "Hello World Today is: ";
 
-    <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text">Email</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-        </div>
-    </div>
+// Prints the day, date, month, year, time, AM or PM
+echo date("l jS \of F Y h:i:s A");
+?>
+<p>
+	<a href="/login-system/login.php">Login</a>
+	<a href="/login-system/register.php">Register</a>
+</p>
+   </div>
 
-    <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text-1">Password</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text-1" type="password" required="required" placeholder="Your Password">
-        </div>
-    </div>
-                    
-    <div class="uk-margin">
-        <button class="uk-button uk-button-default" type="submit">Login</button>
-    </div>                
-
-</form>
-        </div>
-   </div>                                                        
-
-    <!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css" />
-
-<!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js"></script>
-<script src="http://code.jquery.com/jquery-3.3.1.js"></script>                                                                                              
+   <?php require_once "inc/footer.php"; ?>                                                                                      
 </body>
 </html>    
